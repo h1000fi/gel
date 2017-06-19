83c9ddf368267247b426d255da0ce56251acf6bc
kai <huangk05@gmail.com>
2017-06-19 16:50:02 -0500

    Allow two kinds of spacers, one more adhesive than the other

--- monomers.definitions.f
+++ monomers.definitions.f
@@ -10,8 +10,8 @@
 
       integer i, j
 
-      N_poorsol = 2 ! number of different kais
-      N_monomer = 4 
+      N_poorsol = 3 ! number of different kais
+      N_monomer = 5 
 
       ALLOCATE (st_matrix(N_poorsol, N_poorsol)) ! interaction between monomer types in fraction of st, scaled by st-scale during running....
       ALLOCATE (zpol(N_monomer))    ! charge of monomer segment: 1: base, -1: acid, 0:neutral
@@ -31,12 +31,18 @@
 
       st_matrix(1,1)= 0.0 !1.1 !2.70
       st_matrix(2,2)= kaiHO !0.0 !1.0
+      st_matrix(3,3)= kaiHI
 
-      st_matrix(1,2)= kaiHO
-      st_matrix(2,1)= kaiHO
+      st_matrix(1,2)= kaiHI
+      st_matrix(2,1)= kaiHI
+      st_matrix(1,3)= kaiHI
+      st_matrix(3,1)= kaiHI
+      st_matrix(2,3)= kaiHI
+      st_matrix(3,2)= kaiHI
 
       henergy(1) =  pairst !pairst !st_matrix(1,1)
       henergy(2) =  0 !kaiHI !st_matrix(1,2)
+      henergy(3) =  0
 
 ! Segment type 1, pairing monomers
 
@@ -44,23 +50,29 @@
       hydroph(1) = 1
       pKa(1) = 1 ! set any number if zpol = 0...
 
-! Segment type 2, spacers
+! Segment type 2, helper spacers
 
       zpol(2) = 0
       hydroph(2) = 2
       pKa(2) = 1 ! set any number if zpol = 0...
 
-! Segment type 3, charged group
+! Segment type 3, normal spacers
 
-      zpol(3) = zpolHO1 ! 1
-      hydroph(3) = 0
-      pKa(3) = pKaHO1 ! 5.0 ! set any number if zpol = 0....
+      zpol(3) = 0
+      hydroph(3) = 3
+      pKa(3) = 1 ! set any number if zpol = 0...
 
 ! Segment type 4, charged group
 
-      zpol(4) = zpolHO2
+      zpol(4) = zpolHO1 ! 1
       hydroph(4) = 0
-      pKa(4) = pKaHO2
+      pKa(4) = pKaHO1 ! 5.0 ! set any number if zpol = 0....
+
+! Segment type 5, charged group
+
+      zpol(5) = zpolHO2
+      hydroph(5) = 0
+      pKa(5) = pKaHO2
 
       end
 
